eb105f038d4d41174215783a67e9fc12fe589d23
John Crispin <john@phrozen.org>
2020-11-18 08:58:35 +0100

    usync-schema: move files around
    Signed-off-by: John Crispin <john@phrozen.org>

--- generate.sh
+++ generate.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-sed -i "s/\t/        /g" -i *.yml
+sed -i "s/\t/        /g" -i schema/*.yml
 
-yaml2json network.yml network.schema
-yaml2json system.yml system.schema
-yaml2json log.yml log.schema
-yaml2json ntp.yml ntp.schema
-yaml2json ssh.yml ssh.schema
-yaml2json wifi-phy.yml wifi-phy.schema
-yaml2json wifi-ssid.yml wifi-ssid.schema
+yaml2json schema/network.yml network.schema
+yaml2json schema/system.yml system.schema
+yaml2json schema/log.yml log.schema
+yaml2json schema/ntp.yml ntp.schema
+yaml2json schema/ssh.yml ssh.schema
+yaml2json schema/wifi-phy.yml wifi-phy.schema
+yaml2json schema/wifi-ssid.yml wifi-ssid.schema
 ./merge-schema.py
+mkdir -p docs
 jsonschema2md usync.schema.json docs/usync-schema.md
 generate-schema-doc usync.schema.json docs/usync-schema.html
