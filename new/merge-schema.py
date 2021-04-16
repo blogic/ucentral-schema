265793056cdf0cf28b7f1bd1b022e48f548a52dd
Jo-Philipp Wich <jo@mein.io>
2021-04-16 19:41:52 +0200

    schema: improvements
     - Decompose final JSON schema into various reusable sub-schemas within
       the same schema document
    
     - Ensure that each entity schema only describes one instance of the
       entity and push the definitions for collections of entity instances
       to the containing entity, e.g. instead of `ssids: { $ref: ssids }`
       we do have `ssids: { type: array, items: { $ref: ssid } }`
    
     - Various minor typo fixes
    
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- new/merge-schema.py
+++ new/merge-schema.py
@@ -8,6 +8,10 @@ def schema_filename(list):
     file.pop(0)
     return file[0].replace("/", ".") + "yml"
 
+def entity_name(uri):
+    name = uri.replace("https://ucentral.io/schema/v1/", "").rstrip("/")
+    return name.replace("/", ".")
+
 def schema_load(filename):
     with open(filename) as stream:
         try:
@@ -16,16 +20,18 @@ def schema_load(filename):
         except yaml.YAMLError as exc:
             print(exc)
 
-def schema_compile(input, output, tiny):
+def schema_compile(input, output, definitions, tiny):
     for k in input:
         if tiny and k == "description":
             continue
         if isinstance(input[k], dict):
             if k not in output:
                 output[k] = {}
-            schema_compile(input[k], output[k], tiny)
+            schema_compile(input[k], output[k], definitions, tiny)
         elif k == "$ref" and input[k].startswith("https://"):
-            output.update(schema_compile(schema_load(schema_filename(input[k])), {}, tiny))
+            name = entity_name(input[k])
+            definitions[name] = schema_compile(schema_load(schema_filename(input[k])), {}, definitions, tiny)
+            output["$ref"] = "#/definitions/{}".format(name)
         elif k == "$ref" and not tiny:
             output["properties"] = {"reference": {"type": "string"}}
         else:
@@ -34,7 +40,10 @@ def schema_compile(input, output, tiny):
 
 def schema_generate(filename, tiny):
     with open(filename, 'w') as outfile:
-        json.dump(schema_compile(schema_load("ucentral.yml"), {}, tiny), outfile, ensure_ascii = tiny and False or True, indent = tiny and 0 or 4)
+        defs = {}
+        schema = schema_compile(schema_load("ucentral.yml"), {}, defs, tiny)
+        schema["definitions"] = defs
+        json.dump(schema, outfile, ensure_ascii = tiny and False or True, indent = tiny and 0 or 4)
 
 schema_generate('ucentral.schema.json', 1)
 schema_generate('ucentral.schema.pretty.json', 0)
