blogic/ucentral-schema

commit eb4517617e6c18d36870b5666f539e11e8481687

schema: add wifi-steering support
Signed-off-by: John Crispin <john@phrozen.org>

renderer/renderer.uc
@@ -272,6 +272,22 @@ let services = {
 
 		return interfaces;
 	}
+
+	lookup_ssids: function(service) {
+		let ssids = [];
+
+		for (let interface in state.interfaces) {
+			if (!interface.ssids)
+				continue;
+			for (let ssid in interface.ssids) {
+				if (!ssid.services || index(ssid.services, service) < 0)
+					continue;
+				push(ssids, ssid);
+			}
+		}
+
+		return ssids;
+	}
 };
 
 return {

schemareader.uc
@@ -1064,6 +1064,19 @@ function instantiateInterfaceSsid(value) {
 		obj.unicast_conversion = value["unicast-conversion"];
 	}
 
+	function parseServices(value) {
+		assert(type(value) == "array", "Property interface.ssid.services must be of type array");
+
+		return map(value, (item) => {
+			assert(type(item) == "string", "Items of interface.ssid.services must be of type string");
+			return item;
+		});
+	}
+
+	if (exists(value, "services")) {
+		obj.services = parseServices(value["services"]);
+	}
+
 	if (exists(value, "encryption")) {
 		obj.encryption = instantiateInterfaceSsidEncryption(value["encryption"]);
 	}
@@ -1552,10 +1565,48 @@ function instantiateServiceWifiSteering(value) {
 		assert(type(value["network"]) == "string", "Property service.wifi-steering.network must be of type string");
 		obj.network = value["network"];
 	}
+	else {
+		obj.network = "upstream";
+	}
 
-	if (exists(value, "key")) {
-		assert(type(value["key"]) == "string", "Property service.wifi-steering.key must be of type string");
-		obj.key = value["key"];
+	if (exists(value, "assoc-steering")) {
+		assert(type(value["assoc-steering"]) == "bool", "Property service.wifi-steering.assoc-steering must be of type boolean");
+		obj.assoc_steering = value["assoc-steering"];
+	}
+	else {
+		obj.assoc_steering = false;
+	}
+
+	if (exists(value, "required-snr")) {
+		assert(type(value["required-snr"]) == "int", "Property service.wifi-steering.required-snr must be of type integer");
+		obj.required_snr = value["required-snr"];
+	}
+	else {
+		obj.required_snr = 0;
+	}
+
+	if (exists(value, "required-probe-snr")) {
+		assert(type(value["required-probe-snr"]) == "int", "Property service.wifi-steering.required-probe-snr must be of type integer");
+		obj.required_probe_snr = value["required-probe-snr"];
+	}
+	else {
+		obj.required_probe_snr = 0;
+	}
+
+	if (exists(value, "required-roam-snr")) {
+		assert(type(value["required-roam-snr"]) == "int", "Property service.wifi-steering.required-roam-snr must be of type integer");
+		obj.required_roam_snr = value["required-roam-snr"];
+	}
+	else {
+		obj.required_roam_snr = 0;
+	}
+
+	if (exists(value, "load-kick-threshold")) {
+		assert(type(value["load-kick-threshold"]) == "int", "Property service.wifi-steering.load-kick-threshold must be of type integer");
+		obj.load_kick_threshold = value["load-kick-threshold"];
+	}
+	else {
+		obj.load_kick_threshold = 0;
 	}
 
 	return obj;

renderer/templates/services/wifi-steering.uc
@@ -1,0 +1,16 @@
+{% if (wifi_steering.mode == 'local'): %}
+
+# Wifi-Steering service configuration
+set usteer.@usteer[-1].network={{ s(wifi_steering.network) }}
+set usteer.@usteer[-1].key={{ s(wifi_steering.key) }}
+set usteer.@usteer[-1].assoc_steering={{ b(wifi_steering.assoc_steering) }}
+set usteer.@usteer[-1].min_snr={{ wifi_steering.required_snr }}
+set usteer.@usteer[-1].min_connect_snr={{ wifi_steering.required_probe_snr }}
+set usteer.@usteer[-1].roam_scan_snr={{ wifi_steering.required_roam_snr }}
+set usteer.@usteer[-1].load_kick_enabled={{ b(wifi_steering.load_kick_threshold) }}
+set usteer.@usteer[-1].load_kick_threshold={{ wifi_steering.load_kick_threshold }}
+{%   let ssids = services.lookup_ssids("wifi-steering") %}
+{%   for (let ssid in ssids): %}
+add_list usteer.@usteer[-1].ssid_list={{ ssid.name }}
+{%   endfor %}
+{% endif %}

renderer/templates/services/wifi_steering.uc
@@ -1,13 +1,0 @@
-
-# Wifi-Steering service configuration
-
-# TODO work out how to set the network
-
-{% if (wifi_steering.mode == 'local'): %}
-set usteer.@usteer[-1].network={{ s(wifi_steering.network) }}
-set usteer.@usteer[-1].key={{ s(wifi_steering.key) }}
-{% else %}
-delete usteer.@usteer[-1].network
-delete usteer.@usteer[-1].key
-{% endif %}
-

schema/interface.ssid.yml
@@ -65,6 +65,15 @@ properties:
     description:
       Convert multicast traffic to unicast on this BSS.
     type: boolean
+  services:
+    description:
+      The services that shall be offered on this logical interface. These are
+      just strings such as "wifi-steering"
+    type: array
+    items:
+      type: string
+      examples:
+      - wifi-steering
   encryption:
     $ref: "https://ucentral.io/schema/v1/interface/ssid/encryption/"
   multi-psk:

schema/service.wifi-steering.yml
@@ -15,12 +15,32 @@ properties:
     description:
       The network that shall be used to communicate with the other peers.
     type: string
-    examples:
-    - upstream
-  key:
+    default: upstream
+  assoc-steering:
     description:
-      The pre-shared crypto key that units in the cluster use to secure the
-      communication
-    type: string
-    examples:
-    - secret
+      Allow rejecting assoc requests for steering purposes.
+    type: boolean
+    default: false
+  required-snr:
+    description:
+      Minimum required signal level (dBm) for connected clients. If the client
+      will be kicked if the SNR drops below this value.
+    type: integer
+    default: 0
+  required-probe-snr:
+    description:
+      Minimum required signal level (dBm) to allow connections. If the SNR is
+      below this value, probe requests will not be replied to.
+    type: integer
+    default: 0
+  required-roam-snr:
+    description:
+      Minimum required signal level (dBm) before an attempt is made to roam the
+      client to a better AP.
+    type: integer
+    default: 0
+  load-kick-threshold:
+    description:
+      Minimum channel load (%) before kicking clients
+    type: integer
+    default: 0

commit fbac0d8dc1c27559550a1e7e71ffbfb4b008e9b3

renderer: add missing comma
Fixes: eb45176 ("schema: add wifi-steering support")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

renderer/renderer.uc
@@ -271,7 +271,7 @@ let services = {
 		}
 
 		return interfaces;
-	}
+	},
 
 	lookup_ssids: function(service) {
 		let ssids = [];

schemareader.uc
@@ -462,6 +462,76 @@ function instantiateInterfaceIpv4(value) {
 	return obj;
 }
 
+function instantiateInterfaceIpv6Dhcpv6(value) {
+	assert(type(value) == "object", "Property interface.ipv6.dhcpv6 must be of type object");
+
+	let obj = {};
+
+	if (exists(value, "mode")) {
+		assert(type(value["mode"]) == "string", "Property interface.ipv6.dhcpv6.mode must be of type string");
+		assert(value["mode"] in [ "hybrid", "stateless", "stateful", "relay" ], "Property interface.ipv6.dhcpv6.mode must be one of [ \"hybrid\", \"stateless\", \"stateful\", \"relay\" ]");
+		obj.mode = value["mode"];
+	}
+
+	function parseAnnounceDns(value) {
+		assert(type(value) == "array", "Property interface.ipv6.dhcpv6.announce-dns must be of type array");
+
+		return map(value, (item) => {
+			assert(type(item) == "string", "Items of interface.ipv6.dhcpv6.announce-dns must be of type string");
+			return item;
+		});
+	}
+
+	if (exists(value, "announce-dns")) {
+		obj.announce_dns = parseAnnounceDns(value["announce-dns"]);
+	}
+
+	if (exists(value, "filter-prefix")) {
+		assert(type(value["filter-prefix"]) == "string", "Property interface.ipv6.dhcpv6.filter-prefix must be of type string");
+		obj.filter_prefix = value["filter-prefix"];
+	}
+	else {
+		obj.filter_prefix = "::/0";
+	}
+
+	return obj;
+}
+
+function instantiateInterfaceIpv6(value) {
+	assert(type(value) == "object", "Property interface.ipv6 must be of type object");
+
+	let obj = {};
+
+	if (exists(value, "addressing")) {
+		assert(type(value["addressing"]) == "string", "Property interface.ipv6.addressing must be of type string");
+		assert(value["addressing"] in [ "dynamic", "static" ], "Property interface.ipv6.addressing must be one of [ \"dynamic\", \"static\" ]");
+		obj.addressing = value["addressing"];
+	}
+
+	if (exists(value, "subnet")) {
+		assert(type(value["subnet"]) == "string", "Property interface.ipv6.subnet must be of type string");
+		obj.subnet = value["subnet"];
+	}
+
+	if (exists(value, "gateway")) {
+		assert(type(value["gateway"]) == "string", "Property interface.ipv6.gateway must be of type string");
+		obj.gateway = value["gateway"];
+	}
+
+	if (exists(value, "prefix-size")) {
+		assert(type(value["prefix-size"]) == "int", "Property interface.ipv6.prefix-size must be of type integer");
+		assert(value["prefix-size"] <= 64, "Property interface.ipv6.prefix-size must be <= 64");
+		assert(value["prefix-size"] >= 0, "Property interface.ipv6.prefix-size must be >= 0");
+		obj.prefix_size = value["prefix-size"];
+	}
+
+	if (exists(value, "dhcpv6")) {
+		obj.dhcpv6 = instantiateInterfaceIpv6Dhcpv6(value["dhcpv6"]);
+	}
+
+	return obj;
+}
+
 function instantiateInterfaceSsidEncryption(value) {
 	assert(type(value) == "object", "Property interface.ssid.encryption must be of type object");
 
@@ -1301,13 +1371,8 @@ function instantiateInterface(value) {
 		obj.ipv4 = instantiateInterfaceIpv4(value["ipv4"]);
 	}
 
-	function parseIpv6(value) {
-
-		return value;
-	}
-
 	if (exists(value, "ipv6")) {
-		obj.ipv6 = parseIpv6(value["ipv6"]);
+		obj.ipv6 = instantiateInterfaceIpv6(value["ipv6"]);
 	}
 
 	function parseSsids(value) {