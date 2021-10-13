e9d7049b4d8214687d5c8b9ad07fa04cd243083b
Stijn Tintel <stijn@linux-ipv6.be>
2021-10-13 16:40:06 +0300

    generate.sh: generate schemareader.uc before docs
    On systems that don't have generate-schema-doc installed, the
    generate.sh script will exit before generating schemareader.uc. This
    will result in the schemareader.uc file not reflecting the latest
    schema. As outdated documentation is less critical, generate the
    schemareader.uc file before generating the documentation. This should
    reduce the amount of fixup commits needed to update generated files.
    
    Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

--- generate.sh
+++ generate.sh
@@ -7,8 +7,8 @@ set -x
 ./merge-schema.py schema schema ucentral.yml ucentral.schema.pretty.json 0 1
 ./merge-schema.py schema schema ucentral.yml ucentral.schema.full.json 0 0
 ./merge-schema.py state state state.yml ucentral.state.pretty.json 0 1
+./generate-reader.uc  > schemareader.uc
+#./generate-example.uc > input.json
 mkdir -p docs
 generate-schema-doc --config expand_buttons=true ucentral.schema.pretty.json docs/ucentral-schema.html
 generate-schema-doc --config expand_buttons=true ucentral.state.pretty.json docs/ucentral-state.html
-./generate-reader.uc  > schemareader.uc
-#./generate-example.uc > input.json
