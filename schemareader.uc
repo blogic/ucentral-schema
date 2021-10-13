blogic/ucentral-schema

commit 727159535e6512fc7389f69419589775fbd37400

schema: extend QoS schema
Signed-off-by: John Crispin <john@phrozen.org>

schema/service.open-flow.yml
@@ -6,7 +6,7 @@ properties:
     description:
       The CIDR of the OpenFlow controller target.
     type: string
-    uc-format: cidr
+    format: uc-cidr
     example: 192.168.10.1
   mode:
     description:

schemareader.uc
@@ -4823,6 +4823,12 @@ function instantiateServiceOpenFlow(location, value, errors) {
 		let obj = {};
 
 		function parseController(location, value, errors) {
+			if (type(value) == "string") {
+				if (!matchUcCidr(value))
+					push(errors, [ location, "must be a valid IPv4 or IPv6 CIDR" ]);
+
+			}
+
 			if (type(value) != "string")
 				push(errors, [ location, "must be of type string" ]);
 
@@ -5073,6 +5079,20 @@ function instantiateServiceQualityOfService(location, value, errors) {
 	if (type(value) == "object") {
 		let obj = {};
 
+		function parseSelectPort(location, value, errors) {
+			if (type(value) != "string")
+				push(errors, [ location, "must be of type string" ]);
+
+			return value;
+		}
+
+		if (exists(value, "select-port")) {
+			obj.select_port = parseSelectPort(location + "/select-port", value["select-port"], errors);
+		}
+		else {
+			obj.select_port = "WAN1";
+		}
+
 		function parseUploadRate(location, value, errors) {
 			if (type(value) != "int")
 				push(errors, [ location, "must be of type integer" ]);
@@ -5099,6 +5119,149 @@ function instantiateServiceQualityOfService(location, value, errors) {
 		}
 		else {
 			obj.download_rate = 0;
+		}
+
+		function parseReclassifyIngress(location, value, errors) {
+			if (type(value) != "bool")
+				push(errors, [ location, "must be of type boolean" ]);
+
+			return value;
+		}
+
+		if (exists(value, "reclassify-ingress")) {
+			obj.reclassify_ingress = parseReclassifyIngress(location + "/reclassify-ingress", value["reclassify-ingress"], errors);
+		}
+		else {
+			obj.reclassify_ingress = true;
+		}
+
+		function parseClassifier(location, value, errors) {
+			if (type(value) == "array") {
+				function parseItem(location, value, errors) {
+					if (type(value) == "object") {
+						let obj = {};
+
+						function parseDscp(location, value, errors) {
+							if (type(value) != "string")
+								push(errors, [ location, "must be of type string" ]);
+
+							if (!(value in [ "CS0", "CS1", "CS2", "CS3", "CS4", "CS5", "CS6", "CS7" ]))
+								push(errors, [ location, "must be one of \"CS0\", \"CS1\", \"CS2\", \"CS3\", \"CS4\", \"CS5\", \"CS6\" or \"CS7\"" ]);
+
+							return value;
+						}
+
+						if (exists(value, "dscp")) {
+							obj.dscp = parseDscp(location + "/dscp", value["dscp"], errors);
+						}
+						else {
+							obj.dscp = "CS1";
+						}
+
+						function parsePorts(location, value, errors) {
+							if (type(value) == "object") {
+								let obj = {};
+
+								function parseProtocol(location, value, errors) {
+									if (type(value) != "string")
+										push(errors, [ location, "must be of type string" ]);
+
+									if (!(value in [ "any", "tcp", "udp" ]))
+										push(errors, [ location, "must be one of \"any\", \"tcp\" or \"udp\"" ]);
+
+									return value;
+								}
+
+								if (exists(value, "protocol")) {
+									obj.protocol = parseProtocol(location + "/protocol", value["protocol"], errors);
+								}
+								else {
+									obj.protocol = "any";
+								}
+
+								function parsePort(location, value, errors) {
+									if (type(value) != "int")
+										push(errors, [ location, "must be of type integer" ]);
+
+									return value;
+								}
+
+								if (exists(value, "port")) {
+									obj.port = parsePort(location + "/port", value["port"], errors);
+								}
+
+								function parseRangeEnd(location, value, errors) {
+									if (type(value) != "int")
+										push(errors, [ location, "must be of type integer" ]);
+
+									return value;
+								}
+
+								if (exists(value, "range-end")) {
+									obj.range_end = parseRangeEnd(location + "/range-end", value["range-end"], errors);
+								}
+
+								return obj;
+							}
+
+							if (type(value) != "object")
+								push(errors, [ location, "must be of type object" ]);
+
+							return value;
+						}
+
+						if (exists(value, "ports")) {
+							obj.ports = parsePorts(location + "/ports", value["ports"], errors);
+						}
+
+						function parseDns(location, value, errors) {
+							if (type(value) == "array") {
+								function parseItem(location, value, errors) {
+									if (type(value) == "string") {
+										if (!matchFqdn(value))
+											push(errors, [ location, "must be a valid fully qualified domain name" ]);
+
+									}
+
+									if (type(value) != "string")
+										push(errors, [ location, "must be of type string" ]);
+
+									return value;
+								}
+
+								return map(value, (item, i) => parseItem(location + "/" + i, item, errors));
+							}
+
+							if (type(value) != "array")
+								push(errors, [ location, "must be of type array" ]);
+
+							return value;
+						}
+
+						if (exists(value, "dns")) {
+							obj.dns = parseDns(location + "/dns", value["dns"], errors);
+						}
+
+						return obj;
+					}
+
+					if (type(value) != "object")
+						push(errors, [ location, "must be of type object" ]);
+
+					return value;
+				}
+
+				return map(value, (item, i) => parseItem(location + "/" + i, item, errors));
+			}
+
+			if (type(value) != "array")
+				push(errors, [ location, "must be of type array" ]);
+
+			return value;
+		}
+
+		if (exists(value, "classifier")) {
+			obj.classifier = parseClassifier(location + "/classifier", value["classifier"], errors);
 		}
 
 		return obj;

schema/service.quality-of-service.yml
@@ -2,6 +2,12 @@ description:
   This section describes the QoS behaviour of the unit.
 type: object
 properties:
+  select-port:
+    description:
+      The physical network devices that shall be considered the primary uplink interface.
+      All classification and shaping will happen on this device.
+    type: string
+    default: WAN1
   upload-rate:
     description:
       Defines the upload rate of this device. If it is not known or the device is
@@ -14,3 +20,61 @@ properties:
       attached to a shared medium, this value needs to be 0.
     type: integer
     default: 0
+  reclassify-ingress:
+    description:
+      Ignore the ToS/DSCP of incoming packets and reclassify them.
+    type: boolean
+    default: true
+  classifier:
+    description:
+      A list of classifiers. Each classifier will map certain traffic to specific ToS/DSCP
+      values based upon the defined constraints.
+    type: array
+    items:
+      type: object
+      properties:
+        dscp:
+          description:
+            The differentiated services code point that shall be assigned to packet that match
+            the rules of this entry.
+          type: string
+          enum:
+          - CS0
+          - CS1
+          - CS2
+          - CS3
+          - CS4
+          - CS5
+          - CS6
+          - CS7
+          default: CS1
+        ports:
+          description:
+            Each entry defines a layer3 protocol and a port(range) that will be used to match packets.
+          type: object
+          properties:
+            protocol:
+              description:
+                The port match can apply for TCP, UDP or any IP protocol.
+              type: string
+              enum:
+              - any
+              - tcp
+              - udp
+              default: any
+            port:
+              description:
+                The port of this match rule.
+              type: integer
+            range-end:
+              description:
+                The last port of this match rule if it is a port range.
+              type: integer
+        dns:
+          description:
+            Each entry defines a wildcard FQDN. The IP that this resolves to will be used to match packets.
+          type: array
+          items:
+            type: string
+            format: fqdn
+

commit 123ab02113b54960eb8e4bc72da4dc7a309deaf7

open-flow: fix controller property type
The uc-cidr format expects a requires a prefix part, which will not work
when passed to Open vSwitch. Change the type to the newly introduced ip.
This solves the following confusing error with a valid IP:

[E] (In /services/open-flow/controller) Value must be a valid IPv4 or IPv6 CIDR

Fixes: 727159535e65 ("schema: extend QoS schema")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

schema/service.open-flow.yml
@@ -6,7 +6,7 @@ properties:
     description:
       The CIDR of the OpenFlow controller target.
     type: string
-    format: uc-cidr
+    format: ip
     example: 192.168.10.1
   datapath-description:
     description:

schemareader.uc
@@ -4892,8 +4892,8 @@ function instantiateServiceOpenFlow(location, value, errors) {
 
 		function parseController(location, value, errors) {
 			if (type(value) == "string") {
-				if (!matchUcCidr(value))
-					push(errors, [ location, "must be a valid IPv4 or IPv6 CIDR" ]);
+				if (!matchIp(value))
+					push(errors, [ location, "must be a valid IPv4 or IPv6 address" ]);
 
 			}
 