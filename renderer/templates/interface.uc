blogic/ucentral-schema

commit 75c0a3971eb72c6592c3595068bc4d30f23a0dfc

renderer: add wired interfaces to OVS bridge
The renderer currently only adds wireless interfaces to the Open vSwitch
bridge. Also have it add wired interfaces.

Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

renderer/templates/interface.uc
@@ -176,3 +176,11 @@
 	if (interface.captive)
 		include('interface/captive.uc', { name });
 %}
+
+{% if ("open-flow" in interface.services): %}
+{% for (let port in keys(eth_ports)): %}
+add openvswitch ovs_port;
+set openvswitch.@ovs_port[-1].bridge="br-ovs";
+set openvswitch.@ovs_port[-1].port="{{ port }}";
+{% endfor %}
+{% endif %}

commit f679f518bf999b9497201d814c9939c3228555c9

renderer: fix OpenFlow bridge mode
We should only add downstream wired interfaces to the Open vSwitch
bridge. If OpenFlow is enabled on an upstream interface, the interface
should only be added to the Linux bridge.

Fixes: 75c0a3971eb7 ("renderer: add wired interfaces to OVS bridge")
Signed-off-by: Stijn Tintel <stijn@linux-ipv6.be>

renderer/templates/interface.uc
@@ -177,7 +177,7 @@
 		include('interface/captive.uc', { name });
 %}
 
-{% if ("open-flow" in interface.services): %}
+{% if (interface.role == "downstream" && "open-flow" in interface.services): %}
 {% for (let port in keys(eth_ports)): %}
 add openvswitch ovs_port;
 set openvswitch.@ovs_port[-1].bridge="br-ovs";